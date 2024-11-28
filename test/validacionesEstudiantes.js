f46dd7b4f3dd0e6e3fc9c10d14706cc4bb43ea74
Madai Lopez <20233L001106@utcv.edu.mx>
2024-11-28 00:45:41 -0600

    Add files via upload

--- test/validacionesEstudiantes.js
+++ test/validacionesEstudiantes.js
@@ -8,3 +8,22 @@ export function validarNombre(nombre) {
 export function estaEnClase(estudiante, clases) {
     return clases.some(clase => clase.estudiantes.includes(estudiante));
 }
+
+// Validación para el ID del juego (solo números)
+function validarIDJuego(id) {
+    const regex = /^[0-9]+$/;
+    return regex.test(id);
+  }
+  
+  // Validación para la asignación del juego (solo letras y espacios)
+  function validarAsignacionEstudiante(nombre) {
+    const regex = /^[a-zA-Z\s]+$/;
+    return regex.test(nombre);
+  }
+
+
+
+
+
+  
+  export { validarIDJuego, validarAsignacionEstudiante };
