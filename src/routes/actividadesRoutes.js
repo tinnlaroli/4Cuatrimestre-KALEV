fffdfcf05ec491e81271a43834de2c6fa95cc09e
Martin <olivarezmartin714@gmail.com>
2024-12-03 06:36:04 -0600

    fix: import validar token { }

--- src/routes/actividadesRoutes.js
+++ src/routes/actividadesRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const actividadesController = require('../controllers/actividadesController');
-const { validarToken } = require('../middlewares/validarToken');
+const validarToken = require('../middlewares/validarToken');
 
 /**
  * @swagger
